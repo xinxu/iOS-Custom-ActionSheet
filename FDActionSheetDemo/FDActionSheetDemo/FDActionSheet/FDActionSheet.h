b5bd64cf4370621182fcb4ebe427c789ad54bf78
tassar <xinxu0x@gmail.com>
2015-09-18 17:28:50 +0800

    support custom title view and custom font

--- FDActionSheetDemo/FDActionSheetDemo/FDActionSheet/FDActionSheet.h
+++ FDActionSheetDemo/FDActionSheetDemo/FDActionSheet/FDActionSheet.h
@@ -10,13 +10,25 @@
 
 @protocol FDActionSheetDelegate;
 
+@protocol FDActionSheetTitleView <NSObject>
+
+- (NSString *)text;
+
+- (void)setTextColor:(UIColor *)color;
+
+- (void)setFont:(UIFont *)font;
+
+@end
+
 @interface FDActionSheet : UIView
 
 @property (weak, nonatomic) id<FDActionSheetDelegate> delegate;
 @property (strong, nonatomic) NSString *title;
 @property (strong, nonatomic) NSString *cancelButtonTitle;
 
-- (id)initWithTitle:(NSString *)title delegate:(id)delegate cancelButtonTitle:(NSString *)cancelButtonTitle otherButtonTitles:(NSString *)otherButtonTitles, ... NS_REQUIRES_NIL_TERMINATION;
+- (instancetype)initWithTitle:(NSString *)title delegate:(id)delegate cancelButtonTitle:(NSString *)cancelButtonTitle otherButtonTitles:(NSString *)otherButtonTitles, ... NS_REQUIRES_NIL_TERMINATION;
+
+- (instancetype)initWithTitleView:(UIView<FDActionSheetTitleView> *)titleView delegate:(id)delegate cancelButtonTitle:(NSString *)cancelButtonTitle otherButtonTitles:(NSString *)otherButtonTitles, ... NS_REQUIRES_NIL_TERMINATION;
 
 // Show the action sheet at current window
 - (void)show;
@@ -26,6 +38,9 @@
 
 // Set the text Color  and the font size of title. Default the text color is black, and font size is 15
 - (void)setTitleColor:(UIColor *)color fontSize:(CGFloat)size;
+
+// Set the text Color  and the font of title. Default the text color is black, and font size is 15 with system font
+- (void)setTitleColor:(UIColor *)color font:(UIFont *)font;
 
 // Set the title color, background color and font size of button at index. Default the ttitle color is black, background color is white and font size is 14.
 - (void)setButtonTitleColor:(UIColor *)color bgColor:(UIColor *)bgcolor fontSize:(CGFloat)size atIndex:(int)index;
